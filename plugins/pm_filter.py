1411f9d01d404068ea761154cb7f4c4ae67186f8
moviebuzz203 <135233818+moviebuzz203@users.noreply.github.com>
2023-09-13 16:11:15 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -26,11 +26,7 @@ from database.filters_mdb import (
     find_filter,
     get_filters,
 )
-from database.gfilters_mdb import (
-    find_gfilter,
-    get_gfilters,
-    del_allg
-)
+
 import logging
 
 logger = logging.getLogger(__name__)
